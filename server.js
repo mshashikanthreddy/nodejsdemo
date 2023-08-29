0979d291571513d1971bc1bd01c0b64a06ce0085
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2023-08-29 16:28:17 +0530

    adding responses for requests

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    res.write('Shashikanth');
-    res.end()
-
+    const url = req.url;
+    if(url === '/home')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url === '/about')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome to about us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url === '/node')
+    {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome to my node js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else
+    {   
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body><h1>Welcome to server</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 server.listen(4000);
