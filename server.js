b02969a3ee5deb6e31ea0104216eb6d803ab2535
mshashikanthreddy <shashikanthreddymulinti@gmail.com>
2023-09-07 15:32:43 +0530

    body parsing,post request using middlewares

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const http = require('http');
-
 const express = require('express');
+
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended:true}));
 
+/* middleware works on the sequencial order and there is default '/' called automatically. so, if have prefernences to call other routes call at before it.*/
+
+app.use('/add-product',(req, res, next) => {
+    res.send(`<form action="/product" method="POST">Product Name:
+    <input type="text" name="title">size:
+    <input type="number" name="size"><button type="submit">Add Product</button>
+    </form>`);
 });
 
-app.use((req,res,next) => {
-    console.log('In the middleware');
-    res.send('<h1>Hello from Express!</h1>');
+app.post('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
+
+app.use('/',(req, res, next) => {
+    res.send('<h1> Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-     
-//         res.setHeader('Content-Type','text/html');
-//         res.write('<html>');
-//         res.write('<head><title>Enter Message</title></head>');
-//         res.write('<body><h1>Welcome to server</h1></body>');
-//         res.write('</html>');
-//         res.end();
 
-server.listen(4000);
 
+
+app.listen(4000);
 
 
